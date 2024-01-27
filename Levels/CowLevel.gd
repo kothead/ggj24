b47d89639eab2c806c947e4e35ee1ad85092f6d8
DyingDaylight <galeyevaev@gmail.com>
2024-01-27 05:08:16 +0200

    Cow level

---
+++ Levels/CowLevel.gd
@@ -0,0 +1,22 @@
+extends Level
+
+
+var roflFacePacked = load("res://Objects/ROFLFace.tscn")
+
+var detectivePacked = load("res://Objects/Detective.tscn")
+var moneyPacked = load("res://Objects/Money.tscn")
+var picturePacked = load("res://Objects/Picture.tscn")
+
+
+func _on_base_smile_intersected(bodies: Array) -> void:
+	if merge_smiles(bodies, ["FlyingSaucer", "Cow"], detectivePacked):
+		pass
+		
+	if spawn_item(bodies, "FlyingSaucer", false, "Camera", true, picturePacked):
+		pass
+		
+	if merge_smiles(bodies, ["Detective", "Picture"], moneyPacked):
+		pass
+	
+	if merge_smiles(bodies, ["PokerFace", "Money"], moneyPacked):
+		level_completed.emit()
