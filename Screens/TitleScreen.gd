1dcf77030df831e07887cc73257a303a8b62418b
kothead <stopkillingshoggoths@gmail.com>
2024-01-27 08:01:19 +0200

    Start screen

--- Screens/TitleScreen.gd
+++ Screens/TitleScreen.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-
 var gameScreen = preload("res://Screens/GameScreen.tscn")
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(gameScreen)
+func _process(delta):
+	if Input.is_action_just_pressed("Start"):
+		get_tree().change_scene_to_packed(gameScreen)
